da3023f3b23300cabc8a24bb71aec71ea7c03394
mahahahad <mahad.abd2003@gmail.com>
2023-05-19 14:02:05 +0400

    Add tire perpendicular lines

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ class Car {
     this.carWidth = 70;
     this.tireWidth = 19;
     this.tireLength = 38;
-    this.rotationAngle = 1;
+    this.rotationAngle = -45;
 
     // Start off at the center of the screen
     this.x = innerWidth / 2;
@@ -26,52 +26,65 @@ class Car {
     // Clear screen before every redraw
     this.c.clearRect(0, 0, window.innerWidth, window.innerHeight);
 
+    // Draw arc at center of the screen for reference
     this.c.beginPath();
+    this.c.arc(this.x, this.y, 5, 0, 360);
+    this.c.stroke();
+
+    // Vertical car center line
+    this.c.beginPath();
+    this.c.moveTo(this.x, this.y - this.carLength / 2);
+    this.c.lineTo(this.x, this.y + this.carLength / 2);
+    this.c.stroke();
+
     // Bottom Right Tire
+    this.c.beginPath();
     this.c.rect(
-      this.x + this.carWidth / 2,
-      this.y + this.carLength / 2,
+      this.x + this.carWidth / 2 - this.tireWidth / 2,
+      this.y + this.carLength / 2 - this.tireLength / 2,
       this.tireWidth,
       this.tireLength
     );
 
     // Bottom Left Tire
     this.c.rect(
-      this.x - this.carWidth / 2,
-      this.y + this.carLength / 2,
+      this.x - this.carWidth / 2 - this.tireWidth / 2,
+      this.y + this.carLength / 2 - this.tireLength / 2,
       this.tireWidth,
       this.tireLength
     );
 
     let xPos = this.x + this.carWidth / 2;
     let yPos = this.y - this.carLength / 2;
-    let tireHorizontalCenter = xPos + this.tireWidth / 2;
-    let tireVerticalCenter = yPos + this.tireLength / 2;
+    let tireHorizontalCenter = xPos;
+    let tireVerticalCenter = yPos;
 
+    // Rotate the tire
     this.c.translate(tireHorizontalCenter, tireVerticalCenter);
-    this.c.rotate(Math.PI / this.rotationAngle);
+    this.c.rotate((this.rotationAngle * Math.PI) / 180);
     this.c.translate(-tireHorizontalCenter, -tireVerticalCenter);
 
     // Top Right Tire
     this.c.rect(
-      this.x + this.carWidth / 2,
-      this.y - this.carLength / 2,
+      this.x + this.carWidth / 2 - this.tireWidth / 2,
+      this.y - this.carLength / 2 - this.tireLength / 2,
       this.tireWidth,
       this.tireLength
     );
 
+    // Cancel out the rotation for the next redraw
     this.c.translate(tireHorizontalCenter, tireVerticalCenter);
-    this.c.rotate(Math.PI / -this.rotationAngle);
+    this.c.rotate(-(this.rotationAngle * Math.PI) / 180);
     this.c.translate(-tireHorizontalCenter, -tireVerticalCenter);
 
-    xPos = this.x - this.carWidth / 2;
-    yPos = this.y - this.carLength / 2;
+    xPos = this.x - this.carWidth / 2 - this.tireWidth / 2;
+    yPos = this.y - this.carLength / 2 - this.tireLength / 2;
     tireHorizontalCenter = xPos + this.tireWidth / 2;
     tireVerticalCenter = yPos + this.tireLength / 2;
 
     // Rotate the tire
     this.c.translate(tireHorizontalCenter, tireVerticalCenter);
-    this.c.rotate(Math.PI / this.rotationAngle);
+    this.c.rotate((this.rotationAngle * Math.PI) / 180);
     this.c.translate(-tireHorizontalCenter, -tireVerticalCenter);
 
     // Draw the tire
@@ -80,8 +93,60 @@ class Car {
 
     // Cancel out the rotation for the next redraw
     this.c.translate(tireHorizontalCenter, tireVerticalCenter);
-    this.c.rotate(Math.PI / -this.rotationAngle);
+    this.c.rotate(-(this.rotationAngle * Math.PI) / 180);
     this.c.translate(-tireHorizontalCenter, -tireVerticalCenter);
+
+    tireHorizontalCenter = this.x + this.carWidth / 2;
+    tireVerticalCenter = this.y + this.carLength / 2;
+
+    // Bottom tires axis
+    this.c.beginPath();
+    this.c.moveTo(tireHorizontalCenter, tireVerticalCenter);
+    this.c.lineTo(0, tireVerticalCenter);
+    this.c.stroke();
+
+    // Top tires axis
+    // Based on the rotation of the tire
+    // Will always be perpendicular to it
+    tireHorizontalCenter = this.x + this.carWidth / 2;
+    tireVerticalCenter = this.y - this.carLength / 2;
+
+    // Right
+    // this.c.beginPath();
+    // this.c.moveTo(tireHorizontalCenter, tireVerticalCenter);
+    // this.c.lineTo(0, tireVerticalCenter);
+    // this.c.stroke();
+
+    // Left
+    tireHorizontalCenter = this.x - this.carWidth / 2;
+    tireVerticalCenter = this.y - this.carLength / 2;
+    let bottomTireMidpoint = this.y + this.carLength / 2 + this.tireLength / 2;
+
+    if (this.rotationAngle == 0) {
+      this.c.beginPath();
+      this.c.moveTo(tireHorizontalCenter, tireVerticalCenter);
+      this.c.lineTo(0, tireVerticalCenter);
+      this.c.stroke();
+    } else {
+      this.c.beginPath();
+      this.c.moveTo(tireHorizontalCenter, tireVerticalCenter);
+      // this.c.arc(tireHorizontalCenter, tireVerticalCenter, 10, 0, 360);
+      this.c.lineTo(
+        tireHorizontalCenter -
+          this.carLength / Math.tan((Math.PI * -this.rotationAngle) / 180),
+        tireVerticalCenter + this.carLength
+      );
+      this.c.stroke();
+      // alert(this.rotationAngle);
+    }
+    // this.c.beginPath();
+    // this.c.moveTo(tireHorizontalCenter, tireVerticalCenter);
+    // this.c.lineTo(
+    //   bottomTireMidpoint / Math.tan(Math.PI / this.rotationAngle),
+    //   bottomTireMidpoint
+    // );
+    // this.c.stroke();
+    // console.log(this.carLength, this.rotationAngle);
   }
 
   handle() {
@@ -94,9 +159,10 @@ class Car {
           break;
         case "a":
           // this.x -= distance;
-          if (this.rotationAngle > 1.25) return;
+          if (this.rotationAngle <= -45) return;
           if (this.rotationAngle == -1) this.rotationAngle = 1;
-          this.rotationAngle += 0.1;
+          this.rotationAngle -= 15;
+          // this.rotationAngle.toFixed(3);
           // console.log("Move Left");
           break;
         case "s":
@@ -105,9 +171,10 @@ class Car {
           break;
         case "d":
           // this.x += distance;
-          if (this.rotationAngle < -1.25) return;
+          if (this.rotationAngle >= 45) return;
           if (this.rotationAngle == 1) this.rotationAngle = -1;
-          this.rotationAngle -= 0.1;
+          this.rotationAngle += 15;
+          // this.rotationAngle.toFixed(3);
           // console.log("Move Right");
           break;
         default:
